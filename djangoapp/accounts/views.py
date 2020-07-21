d2efb14284bb610575cea537f93588330e123cb2
KzZe-Sama <lilmisstreebox@gmail.com>
2020-07-21 17:51:21 +0545

    added auth session and redirects to login page if user isnot valid

--- djangoapp/accounts/views.py
+++ djangoapp/accounts/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import LoginForm
+from django.http import HttpResponse
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def login_view(request):
-    form=LoginForm()
+    if request.method == "POST":
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            user=authenticate(username=form.cleaned_data['username'],password=form.cleaned_data['password'])
+            if user:
+                    #user found
+                login(request,user)
+                return redirect('/accounts/profile/')
+            else:
+                return dec
+    elif request.method == "GET":
+        form=LoginForm()
     return render(request,'accounts/login.html',{'form':form})
 
+@login_required()
 def profile_view(request):
     return render(request,'accounts/profile.html')
